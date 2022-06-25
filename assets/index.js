7e070029731284e4a4aca7df7d5a41ffbeead2ea
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-25 23:30:29 +0300

    add our leaders

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,11 @@
 const API_URL = "https://api.scripture.api.bible/v1/swagger.json"
+const bibleElement = document.querySelector(".bible");
 
 async function getBibleApi(){
     const response = await fetch(API_URL);
     const json = await response.json();
-    console.log(json);
+    console.log(json.AudioBible);
 }
 getBibleApi();
+
+json.AudioBible.forEach()
