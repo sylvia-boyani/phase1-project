9740e2eb733f59d3ed01d7b7857deea13c92e399
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-25 16:22:04 +0300

    starting on JS

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+function donateNow(){
+    let d = document.getElementById("donate");
+    if (d.style.display === "none") {
+        d.style.display = "block";
+    }
+    else{
+        d.style.display = "none"
+    }
+}
